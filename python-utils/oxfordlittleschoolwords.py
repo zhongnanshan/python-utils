5628445233c70fd25dc0f6c3936e51c204b084a1
张恒 <beowulfbhz@hotmail.com>
2017-11-11 09:43:01 +0800

    create words1a5b.txt file. And manual fixed the file.

--- python-utils/oxfordlittleschoolwords.py
+++ python-utils/oxfordlittleschoolwords.py
@@ -1,6 +1,6 @@
 import re, os
 
-def main():
+def create_words1a5b_file():
     words1a3bfile = os.path.join(os.getcwd(), 'data\\牛津小学英语1A_3B.txt')
     words1a5bfile = os.path.join(os.getcwd(), 'data\\牛津小学英语1A_5B.txt')
 
@@ -17,5 +17,11 @@ def main():
             wordlist = re.split(r'\t+', line)
             words_1a5b.append(wordlist[0])
             print(wordlist[0])
+
+    with open('data\\words1a5b.txt', 'w', encoding='utf-8') as fd:
+        fd.writelines([word.strip(' ')+'\n' for word in words_1a5b])
+
+def main():
+    create_1a5b_file()
 
 main()
